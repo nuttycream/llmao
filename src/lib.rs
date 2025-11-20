81f4e47f6e5e8f888d849373d12e0c8032081987
nuttyCream <issanutty@gmail.com>
2025-11-19 21:02:12 -0800

    feat(core): Add core modules for extract, generate, and tool

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub mod extract;
+pub mod generation;
+pub mod tool;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum ErrorKind {
+    Other,
 }
