24ea6b9986956fd63c1da4fb5c1ffba1baa106f6
nuttyCream <issanutty@gmail.com>
2025-11-28 14:19:34 -0800

    refactor(extract): Introduce robust error handling for extract

--- src/extract.rs
+++ src/extract.rs
@@ -1,1 +1,54 @@
-pub trait Extract {}
+pub trait Error: std::fmt::Debug {
+    fn kind(&self) -> ErrorKind;
+}
+
+/// Extract error kinds
+///
+/// This represents set of extraction errors
+/// some of which ripped from `rig-core`
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum ErrorKind {
+    /// No data extracted from Provider Response
+    NoData,
+
+    /// Failed to deserialize the data
+    DeserializationFailed,
+
+    /// Provider response violated the schema constraints
+    /// for structured outputs
+    BadSchema,
+}
+
+impl std::fmt::Display for ErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ErrorKind::NoData => write!(f, "No data extracted"),
+            ErrorKind::DeserializationFailed => write!(f, "Failed to deserialize data"),
+            ErrorKind::BadSchema => write!(f, "Response violated schema constraints"),
+        }
+    }
+}
+
+impl Error for ErrorKind {
+    #[inline]
+    fn kind(&self) -> ErrorKind {
+        *self
+    }
+}
+
+/// Extraction eerror type trait.
+///
+/// This just defines the error type, to be used by the other traits.
+pub trait ErrorType {
+    /// Error type
+    type Error: Error;
+}
+
+impl<T: ErrorType + ?Sized> ErrorType for &mut T {
+    type Error = T::Error;
+}
+
+// todo impl extract
+// decide whether or not, we should have
+// serde for deserialization generic
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,3 @@
 
 pub mod extract;
 pub mod params;
-
-pub trait Error: std::fmt::Debug {
-    fn kind(&self) -> ErrorKind;
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-#[non_exhaustive]
-pub enum ErrorKind {
-    Other,
-}
