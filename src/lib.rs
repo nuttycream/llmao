735603041fd57a86fb24ff08879ca3e13cc69de3
nuttyCream <issanutty@gmail.com>
2025-11-29 01:52:35 -0800

    feat(provider): Add core Provider trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,17 @@
 
 pub mod extract;
 pub mod params;
+
+/// Provider trait
+///
+/// Essentially the core trait, all LLM providers must at
+/// least implement this once, and create it's error types
+/// Ideally, this would include Network/HTTPS requests and
+/// Provider specific errors.
+pub trait Provider {
+    type Error;
+}
+
+impl<T: Provider + ?Sized> Provider for &mut T {
+    type Error = T::Error;
+}
